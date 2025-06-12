88d72b1483dd99981af4df48d0e6d5e6c93b8964
Florian <73788253+LeTaitre@users.noreply.github.com>
2025-06-12 11:51:11 +0200

    fix statio

--- etats_stationnaires.py
+++ etats_stationnaires.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.linalg import eigh  # Solveur pour matrices Hermitiennes
+from scipy.linalg import eigh  
 
 dx = 0.001
 L = 2.0
@@ -12,17 +12,17 @@ V = np.zeros(nx)
 V[(x >= 0.8) & (x <= 0.9)] = V0
 
 # Construction du Hamiltonien (discrétisation du Laplacien)
-diag = np.ones(nx) * (2 / dx**2)
-off_diag = np.ones(nx - 1) * (-1 / dx**2)
+main_diag = -2 * np.ones(nx)
+off_diag = np.ones(nx - 1)
+laplacien = (np.diag(main_diag) + np.diag(off_diag, 1) + np.diag(off_diag, -1)) / dx**2
 
-H = np.diag(diag) + np.diag(off_diag, k=1) + np.diag(off_diag, k=-1)
-H += np.diag(V)
+H = -0.5 * laplacien + np.diag(V)
 
 num_etats = 10  # Nombre d’états stationnaires à extraire
-energies, etats = eigh(H, subset_by_index=(0, num_etats-1))
+energies, etats = eigh(H)
 
 # Normalisation des états propres
-etats_normalises = etats / np.sqrt(np.sum(np.abs(etats)**2, axis=0) * dx)
+etats_normalises = etats / np.sqrt(np.sum(np.abs(etats)**2) * dx)
 
 # Affichage des états stationnaires
 plt.figure(figsize=(10, 6))
